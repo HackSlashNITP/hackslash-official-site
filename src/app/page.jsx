38daf7c8d2729f9e1329c29096235f9eeeb37534
VasuOOCh <vasuc.ug23.cs@nitp.ac.in>
2024-08-16 11:56:55 +0530

    changed js -> jsx and minor improvements

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,10 @@
-
 // This is page is for home page
-
-import { example } from "@/lib/data";
-
 
 export default async function Home() {
 
-  const data = await example();
-  
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data}
+      <h1>Hello world</h1>
     </main>
   );
 }
--- src/lib/db.js
+++ src/lib/db.js
@@ -12,6 +12,8 @@ export const connectToDb = async () => {
         const db = await mongoose.connect('mongodb://127.0.0.1:27017/hackslash'); // this is just for now, we will use altas in future
         console.log("New connection | Connected to DB");
         connection.isConnected = db.connections[0].readyState;
+        console.log(connection);
+        
         
     } catch (error) {
         console.log(error);
