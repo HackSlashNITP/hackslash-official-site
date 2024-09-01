b807518ebe3e8a442f5bb4ef7f377a6c72b08f41
Sanjeet4567 <156511947+Sanjeet4567@users.noreply.github.com>
2024-09-02 00:56:19 +0530

    Gallery Created

---
+++ src/app/gallery/[event]/[id]/page.jsx
@@ -0,0 +1,28 @@
+import { groupList } from "@/assets/cats";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+export default function Event({params}){
+    const title=params.event.replaceAll("%20"," ")
+    const index=groupList.filter(group=>group.title===title)
+    if(!index){
+        notFound()
+    }
+    
+    const group=index[0]
+    if(parseInt(params.id)>=group.src.length){
+        notFound()
+    }
+    
+
+    return(
+        <>
+        <div className="flex justify-center items-center h-[100vh]">
+            <Image
+            src={group.src[parseInt(params.id)]}
+            alt={`${title}`}
+            className="h-[70vh] w-auto object-contain"
+            />
+        </div>
+        </>
+    )
+}
