6d1a6492e2a8059f07772b9db94e9d86ca80812a
sahityaaryan <sahityaaryansingh@gmail.com>
2025-01-26 03:34:21 +0530

    Removed the office bearers section from the front page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,85 +1,18 @@
 import { verifyToken } from "@/lib/actions";
 import Image from "next/image";
 import Card from "@/components/Card";
-import { connectToDb } from "@/lib/db";
-import { Event } from "@/lib/models";
+// import { connectToDb } from "@/lib/db";
+// import { Event } from "@/lib/models";
 import Link from "next/link";
 import FormComponent from "@/components/FormComponent";
 import CoordinatorCard from "@/components/CoordinatorCard";
 
-const coordinators = [
-  {
-    name: "Abdul SHUBHAN",
-    role: "Lead",
-    image: "/leads/Subhan.jpeg",
-    linkedin: "https://www.linkedin.com/in/abdulsubhan12/",
-    github: "#",
-  },
-  {
-    name: "Sahitya Aryan",
-    role: "Web Lead",
-    image: "/leads/SAHITYA_ARYAN.png",
-    linkedin: "https://www.linkedin.com/in/sahityaaryan/",
-    github: "#",
-  },
-  {
-    name: "Divyansh Gupta",
-    role: "Web Co-Lead",
-    image: "/leads/DIVYANSH_GUPTA.jpg",
-    linkedin: "https://www.linkedin.com/in/divyansh-gupta-7a71b8250/",
-    github: "#",
-  },
-  {
-    name: "Devendra Singh",
-    role: "Flutter Lead",
-    image: "/leads/FlutterLead.png",
-    linkedin:
-      "https://www.linkedin.com/in/devendra-singh-ba1917257/github.com/dev04sa",
-    github: "#",
-  },
-  {
-    name: "Piyush Singh",
-    role: " Flutter Co-Lead",
-    image: "/leads/FlutterCoLead.jpeg",
-    linkedin: "https://www.linkedin.com/in/piyuush-singh/",
-    github: "#",
-  },
-  {
-    name: "Chirag Kumar",
-    role: "Kotlin Lead",
-    image: "/leads/CHIRAG_KUMAR.jpg",
-    linkedin:
-      "https://www.linkedin.com/in/chirag-kumar-rai-bbb009271?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    github: "#",
-  },
-  {
-    name: "Shiva Singh Begari",
-    role: "AI/ML Lead",
-    image: "/leads/SHIVA_SINGH_BAGRI.jpg",
-    linkedin: "https://www.linkedin.com/in/shiva-singh-bagri-060089174",
-    github: "#",
-  },
-  {
-    name: "Govind Singh",
-    role: "Blockchain ",
-    image: "/leads/GOVIND_SINGH.jpg",
-    linkedin: "https://www.linkedin.com/in/govind-singh-a925471a1",
-    github: "#",
-  },
-  {
-    name: "Bobbala Maheedhar",
-    role: "SigSTP Lead",
-    image: "/leads/Bobbala_Maheedhar.jpg",
-    linkedin: "https://www.linkedin.com/in/maheedhar-bobbala-9739a1276/",
-    github: "#",
-  },
-];
 
 export default async function Home() {
-  await connectToDb();
-  const events = await Event.find();
+  // await connectToDb();
+  // const events = await Event.find();
 
-  const upcomingEvents = events.filter((event) => event.eventDate > new Date());
+  // const upcomingEvents = events.filter((event) => event.eventDate > new Date());
   return (
     <main className="flex flex-col gap-4 px-4 sm:px-8 lg:px-0">
       <div className="flex flex-col w-full max-w-[1266px] mx-auto items-center space-y-10 py-8">
@@ -242,13 +175,13 @@ export default async function Home() {
       {/* Upcoming Events */}
       <section className="py-6 bg-black">
         <div className="px-12">
-          {upcomingEvents.length > 0 && (
+          {/* {upcomingEvents.length > 0 && (
             <h2 className="text-3xl md:text-5xl font-medium text-center text-white my-8 md:my-16">
               Upcoming Events
             </h2>
-          )}
+          )} */}
           <div className="flex flex-wrap justify-center gap-6 md:gap-8">
-            {upcomingEvents.length > 0 &&
+            {/* {upcomingEvents.length > 0 &&
               upcomingEvents.slice(0, 3).map((event) => (
                 <div
                   key={event._id}
@@ -280,30 +213,10 @@ export default async function Home() {
                     </div>
                   </div>
                 </div>
-              ))}
+              ))} */}
           </div>
         </div>
       </section>
-
-      {/*Coordinators Section*/}
-      <div className="bg-black min-h-screen flex flex-col items-center">
-        <h1 className="text-white text-6xl font-bold mb-12 text-center">OFFICE BEARERS</h1>
-        <div className="grid gap-10 sm:gap-40 mt-10 sm:mt-20">
-          {/* First Row: 1-2 Cards on Mobile, 2 Cards on Larger Screens */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-20 justify-center">
-            {coordinators.slice(0, 2).map((coordinator, index) => (
-              <CoordinatorCard key={index} {...coordinator} />
-            ))}
-          </div>
-
-          {/* Subsequent Rows: 2 Columns on Mobile, 4 Columns on Larger Screens */}
-          <div className="mt-8 grid grid-cols-2 sm:grid-cols-4 gap-x-5 sm:gap-x-11 gap-y-20 sm:gap-y-40 justify-center">
-            {coordinators.slice(2).map((coordinator, index) => (
-              <CoordinatorCard key={index + 2} {...coordinator} />
-            ))}
-          </div>
-        </div>
-      </div>
 
       {/* Section 04 */}
       <div className="w-11/12 mx-auto border-t border-gray-700 mt-20 "></div>
